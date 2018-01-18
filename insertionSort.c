a54a57b4d91cbe295f1a67b907ffa79024b8f6c1
Asim Maharjan <maharjan291@gmail.com>
2018-01-18 23:32:56 +0545

    Fixed insertion Sort

--- insertionSort.c
+++ insertionSort.c
@@ -1,4 +1,10 @@
-//IMPLEMENTATION OF THE SELECTION SORT IN C
+//IMPLEMENTATION OF Insertion SORT ALGORITHM IN C
+//ALSO PRINTS EVERY STEP OF THE SORT
+//PRINTARRAY FUNCTION PRINTS THE ARRAY OF THE GIVEN LENGTH
+//CHECKSORT CHECKS IF THE GIVEN ARRAY IS SORTED IN ASCENDING ORDER
+//CHECKSORT RERTURNS VALUE TRUE(1) IF ARRAY IS SORTED AND FALSE(0) IF IT IS NOT
+//ALL THE ARRAY RELATED FUNCTIONS TAKE TWO ARGUMENTS:: 1ST IS THE ARRAY AND SECOND IS THE LENGTH OF THE ARRAY
+
 
 #include <stdio.h>
 #define MAXLENGTH 100
@@ -9,7 +15,7 @@
 void insertionSort(int [],int);
 void printArray(int [], int);
 int checkSort(int [],int);
-void swap(int *,int *);
+void swap(int *, int * );
 
 void main(){
 	int x[MAXLENGTH],i=0,c;
@@ -24,32 +30,34 @@ void main(){
 }
 
 void insertionSort(int a[],int length){
-	int i,j,temp,min,minIndex;
-
+	int i,j,minIndex,temp;
+	
 	for( i = 0;i<length;i++)		//PRINTS THE UPPER MENU OF INDEXES
 		printf("Index %d|",i);
 	printf("\n");
 
-	for ( i = 0 ; i<length-1 ; i++ ){
-		min = a[i];
-		LONGLINE;
+	for ( i=0; i<length-1;i++){
+			minIndex = i;
+			LONGLINE;
+		for ( j = i+1; j<length; j++){
 			if ( checkSort(a,length) == TRUE ) // CHECK IF SORTED BEFORE EACH SWAP
 				return;
-		for ( j = i+1;j<length;j++){
-			if ( a[j] < min ){
+			if ( a[j]<a[minIndex]){
 				minIndex = j;
-				min = a[j];
 			}
+			
 		}
-		swap(&a[i],&a[minIndex]);
-		printArray(a,length);  //PRINTS THE ARRAY
+			swap(&a[i],&a[minIndex]);
+			printArray(a,length);  //PRINTS THE ARRAY
 	}
 }
 
-void swap(int *a,int *b){
+
+void swap(int *a, int *b ){
 	int temp = *a;
 	*a = *b;
 	*b = temp;
+
 }
 
 void printArray(int x[],int length){
